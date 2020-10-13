248ff93099c4c5a9151f83ac5781af583dfcdb81
Mihail Bondarenco <441044@student.fontys.nl>
2020-10-13 11:29:02 +0200

    test(sensorValidationService) add tests that mock the server uptime to be over 5 minutes

--- tests/sensorValidationService.test.js
+++ tests/sensorValidationService.test.js
@@ -5,6 +5,17 @@ const {getSensorEntries} = require('../services/influxDbPersistanceService')
 
 const service = require('../services/sensorValidationService')
 
+
+let sampleInvalidEntry
+
+beforeAll(() =>{
+    sampleInvalidEntry ={
+        loc_x: 10,
+        loc_y: 10,
+        floor: 11,
+    }
+})
+
 test('exists', () =>{
     expect(service.reportFaultyMeasurement).toBeTruthy()
 })
@@ -13,11 +24,7 @@ test('Report first faulty entry results in true', async () => {
 
     getSensorEntries.mockResolvedValue([])
 
-    expect(await service.reportFaultyMeasurement({
-        loc_x: 10,
-        loc_y: 10,
-        floor: 11,
-    })).toBeTruthy()
+    expect(await service.reportFaultyMeasurement(sampleInvalidEntry)).toBeTruthy()
 })
 
 test('one faulty entry + more valid entries results in true', async () => {
@@ -29,11 +36,7 @@ test('one faulty entry + more valid entries results in true', async () => {
         {is_valid: 'true'},
     ])
 
-    expect(await service.reportFaultyMeasurement({
-        loc_x: 10,
-        loc_y: 10,
-        floor: 11,
-    })).toBeTruthy()
+    expect(await service.reportFaultyMeasurement(sampleInvalidEntry)).toBeTruthy()
 })
 
 test('The correct coordinates are being requested', async () => {
@@ -44,6 +47,8 @@ test('The correct coordinates are being requested', async () => {
         loc_x: 1,
         loc_y: 2,
         floor: 11,
+        value: 25,
+        paramaeter: 'temperature',
     })
 
     const requestedLoc = getSensorEntries.mock.calls[0][0]
@@ -53,18 +58,67 @@ test('The correct coordinates are being requested', async () => {
     expect(requestedLoc.floor).toBe(11)
 })
 
-
-
-
 test('Report second faulty entry results in false', async () => {
 
     getSensorEntries.mockResolvedValue([
         {is_valid: 'false'}
     ])
 
-    expect(await service.reportFaultyMeasurement({
-        loc_x: 10,
-        loc_y: 10,
-        floor: 11,
-    })).toBeFalsy()
+    expect(await service.reportFaultyMeasurement(sampleInvalidEntry)).toBeFalsy()
+})
+
+describe('Tests when uptime of server is > 5min', () => {
+    beforeEach( () => {
+        jest.spyOn(process, 'uptime').mockImplementation(() => 6 * 60 * 1e9) // 6 minutes
+    })
+
+    test('Report first faulty entry results in false', async () => {
+
+        getSensorEntries.mockResolvedValue([])
+    
+        expect(await service.reportFaultyMeasurement(sampleInvalidEntry)).toBe(false)
+    })
+
+    test('one faulty entry + four valid entries results in false', async () => {
+
+        getSensorEntries.mockResolvedValue([
+            {is_valid: 'true'},
+            {is_valid: 'true'},
+            {is_valid: 'true'},
+            {is_valid: 'true'},
+        ])
+    
+        expect(await service.reportFaultyMeasurement(sampleInvalidEntry)).toBe(false)
+    })
+
+    test('one faulty entry + 18 valid entries results in false', async () => {
+
+        getSensorEntries.mockResolvedValue(new Array(18).fill({is_valid: 'true'}))
+    
+        expect(await service.reportFaultyMeasurement(sampleInvalidEntry)).toBe(false)
+    })
+
+    test('one faulty entry + 19 valid entries results in true', async () => {
+
+        getSensorEntries.mockResolvedValue(new Array(19).fill({is_valid: 'true'}))
+    
+        expect(await service.reportFaultyMeasurement(sampleInvalidEntry)).toBe(true)
+    })
+
+    test('one faulty entry + 20 valid entries results in true', async () => {
+
+        getSensorEntries.mockResolvedValue(new Array(20).fill({is_valid: 'true'}))
+    
+        expect(await service.reportFaultyMeasurement(sampleInvalidEntry)).toBe(true)
+    })
+
+    test('one faulty entry + 18 valid + 1 invalid results in false', async () => {
+
+        const EntryCacheMock = new Array(19).fill({is_valid: 'true'})
+        EntryCacheMock.push({is_valid: false})
+
+        getSensorEntries.mockResolvedValue(EntryCacheMock)
+    
+        expect(await service.reportFaultyMeasurement(sampleInvalidEntry)).toBe(false)
+    })
 })
